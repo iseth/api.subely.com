4a5a5f090bda8dd870e71d7e15ed8179181f6c5b
mrj0909 <site.testing.1234@gmail.com>
2018-02-20 15:24:17 +0500

    Included Access Origin headers

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -60,7 +60,7 @@ $app->singleton(
 */
 
 $app->middleware([
-   // App\Http\Middleware\ExampleMiddleware::class,
+    App\Http\Middleware\ExampleMiddleware::class,
 	\LucaDegasperi\OAuth2Server\Middleware\OAuthExceptionHandlerMiddleware::class
 ]);
 
