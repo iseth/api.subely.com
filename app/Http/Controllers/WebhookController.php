806a4340476f7d82ba0b9631b75593d821971621
mrj0909 <site.testing.1234@gmail.com>
2018-02-22 20:07:25 +0500

    check for error

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -302,6 +302,7 @@ class WebhookController extends Controller{
 		  	try{
 
 				$inner_files = $client->listFolder($full_file_name);
+				dd($inner_files);
 				$iterator = 1;
 
 			} catch (\Exception $e) {
