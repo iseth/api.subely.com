74acd28315079881ad7323ff62f6e6b49cf7edaf
mrj0909 <site.testing.1234@gmail.com>
2018-02-24 19:16:45 +0500

    subcription issue fixed

--- app/Http/Controllers/StripeController.php
+++ app/Http/Controllers/StripeController.php
@@ -88,9 +88,6 @@ class StripeController extends Controller{
                 ]);
 
 
-                dd($subscription['status']);
-
-
               /* $charge = $stripe->charges()->create([
                     'customer' => $customer['id'],
                     'currency' => 'USD',
