dbd6c4571e7b289d098d924447916bd5c27da32d
mrj0909 <site.testing.1234@gmail.com>
2018-02-24 19:06:16 +0500

    stripe issue fixed

--- app/Http/Controllers/StripeController.php
+++ app/Http/Controllers/StripeController.php
@@ -84,9 +84,7 @@ class StripeController extends Controller{
 
 
                 $subscription = $stripe->Subscriptions()->create($customer['id'],[
-                    'items'  =>  [ 
-                        'plan' => $plan_selected->name
-                    ],
+                    'plan' => $plan_selected->name,
                     'billing' => 'send_invoice',
                     'description' => 'Subscribed to '.$plan_selected->name.' plan',
                 ]);
