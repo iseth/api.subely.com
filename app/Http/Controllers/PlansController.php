36f01135ded5634a8a9406e760500c630a40e099
mrj0909 <site.testing.1234@gmail.com>
2018-02-26 16:03:34 +0500

    fix jquery issues

--- app/Http/Controllers/PlansController.php
+++ app/Http/Controllers/PlansController.php
@@ -17,7 +17,7 @@ class PlansController extends Controller{
 
 		if($subscription_exist != null)
 		{
-			$check_plan = DB::table('plans')->where('plan_id','=',$subscription_exist);
+			$check_plan = DB::table('plans')->where('plan_id','=',$subscription_exist)->first();
 		}
 
 		$plans['plans'] = $all_plans;
